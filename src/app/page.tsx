40ddf2b3e22f0f9cecd70df5510dd7b96e992273
Miles Wells <mileswells93@gmail.com>
2025-06-21 19:55:26 -0400

    remove button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@mantine/core';
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Next Template</h1>
-			<Button variant="filled">Yay</Button>
 		</div>
 	);
 }
